b8ea247896787cadedc16a4f8121ebeba490549a
bataashi kingyo <bataashi@example.com>
2018-12-12 18:30:42 +0900

    Lesson6: React練習コミット1。太郎と花子。

--- client/components/Hello.tsx
+++ client/components/Hello.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 
-export default class Hello extends React.Component {
+interface Props {
+  initialName: string;
+}
+interface State {
+  name: string;
+}
+export default class Hello extends React.Component<Props, State> {
+  state: State = {
+    name: this.props.initialName,
+  };
+  setNameTaro = (): void => {
+    this.setState({ name: '太郎' });
+  }
+  setNameHanako = (): void => {
+    this.setState({ name: '花子' });
+  }
   render(): JSX.Element {
+    const { name } : State = this.state;
     return (
-      <div>Hello React!</div>
+      <div>
+        <button type="button" onClick={this.setNameTaro}>太郎</button>
+        <button type="button" onClick={this.setNameHanako}>花子</button>
+        <div>Hello {name}!</div>
+      </div>
     );
   }
 }
--- client/index.tsx
+++ client/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 import Hello from './components/Hello';
 
 ReactDOM.render(
-  <Hello />,
+  <Hello initialName="React" />,
   document.getElementById('app'),
 );
