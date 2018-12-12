9f0b675e867f724d2986404b69ca0c9e7595519e
bataashi kingyo <bataashi@example.com>
2018-12-12 20:54:56 +0900

    React練習コミット3。フォーム。

--- client/components/Hello.tsx
+++ client/components/Hello.tsx
@@ -24,15 +24,13 @@ export default class Hello extends React.Component<Props, State> {
       input: e.target.value,
     });
   }
-  setNameFromInput = (): void => {
+  setNameFromInput = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
     const { input } : State = this.state;
     this.setState({
       name: input,
       input: '',
     });
-  }
-  handleInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>): void => {
-    if (e.key === 'Enter') this.setNameFromInput();
   }
   render(): JSX.Element {
     const { name, input } : State = this.state;
@@ -42,9 +40,10 @@ export default class Hello extends React.Component<Props, State> {
           <button type="button" onClick={this.returnSetName('太郎')}>太郎</button>
           <button type="button" onClick={this.returnSetName('花子')}>花子</button>
         </div>
-        <input type="text" value={input} onChange={this.handleInputChange}
-          onKeyPress={this.handleInputKeyPress}/>
-        <button onClick={this.setNameFromInput}>Hello!</button>
+        <form onSubmit={this.setNameFromInput}>
+          <input type="text" value={input} onChange={this.handleInputChange} />
+          <button type="submit">Hello!</button>
+        </form>
         <div>Hello {name}!</div>
       </div>
     );
